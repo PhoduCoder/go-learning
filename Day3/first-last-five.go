2dae6e5ddd53a72730e0d8eb1ca615d48c9287ea
Gaurav <gauravprashar17@gmail.com>
2024-09-07 10:35:36 -0400

    Added slices internals

--- Day3/first-last-five.go
+++ Day3/first-last-five.go
@@ -5,11 +5,13 @@ import (
 )
 
 func main(){
-	slice1 := []int{2,4,6,8,10,12,14,16,18,20}
+	slice1 := []int{2,4,6,8,10,12,14,16}
 
 	//Print first five
 	fmt.Printf("The first five elements are %v, %v\n", slice1[0:5], slice1[:5])
 
+	//  s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	//  m += fmt.Sprintln("Last  :", s[len(s)-1], s[len(s)-1:len(s)], s[len(s)-1:])
 	//Print last five
 	fmt.Printf("The last five elements are %v\n", slice1[(len(slice1)-5):len(slice1)])
 }
---
+++ Day3/slices-internal.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+//We will talk about Go slices and how it works internally
+// Slices also have a hidden array to it 
+//Size of slice is determined by len
+//while size of hidden array is determined by cap
+//When we used append function, depending on the capacity of the 
+//underlying hidden array, Go in the background adds the new elements to 
+// this undelying array if there is a space in hidden array.
+// other wise Go copies the elements into a new hidden array and then
+//points the slice to the new hidden array
+
+//also note that there is a starting point for the slice
+//which is almost always the starting point of the array
+//unless the slice was created by taking the last n elements of the array
+
+func main(){
+	array1 := [6]int{28,29,25,30,31,34}
+
+	slice1 := array1[1:5]
+	slice2 := array1[:5]
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", slice1, len(slice1), cap(slice1))
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", slice2, len(slice2), cap(slice2))
+
+	new_slice := append(slice1, 35, 36,37,38,39,40,41,42,43,44)
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", new_slice, len(new_slice), cap(new_slice))
+
+	//Now it is possible to control the hidden array 
+//by using the make function
+//in make function when creating a slice, make(<sliceType>, <length>, <capacity>)
+//capacity is optional but used for the hidden array length
+
+
+	new_sl := make([]int, 4, 6)
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", new_sl, len(new_sl), cap(new_sl))
+
+	app_sl := append(new_sl,25,26)
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", app_sl, len(app_sl), cap(app_sl))
+	app_sl_2 := append(app_sl,27)
+	fmt.Printf("The slice is %v and the length is %v and capacity is %v\n", app_sl_2, len(app_sl_2), cap(app_sl_2))
+
+}
+
+
+
+
+
