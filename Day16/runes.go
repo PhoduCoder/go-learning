110a14ae0b6210208d725eec270bd7c43924af35
Gaurav <gauravprashar17@gmail.com>
2025-02-27 22:16:20 -0500

    Added char in Go explanation

---
+++ Day16/runes.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ch rune = 'A'      // Single quotes for a rune
+	fmt.Println(ch)        // Outputs: 65 (ASCII code of 'A')
+	fmt.Printf("%c\n", ch) // Outputs: A
+}
