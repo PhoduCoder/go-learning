4ef7bab5493d81f9360f6d67fe42532e594fff15
Gaurav <gauravprashar17@gmail.com>
2026-03-31 16:24:53 -0400

    Add example of map declaration and manipulation
    Implement a map with initial values and demonstrate updates.

---
+++ Maps/map1.go
@@ -0,0 +1,22 @@
+package main
+
+func main(){
+
+  //Declare a map 
+  m := map[string]int{
+    "apple":  5,
+    "banana": 3,
+}
+
+m1 := make(map[string]int)
+
+m1["apple"] = 5     // add
+m1["banana"] = 3
+m1["apple"] = 10    // update
+
+value := m1["apple"]
+fmt.Println(value)
+
+fmt.Prinln(m["banana"]
+  
+}
